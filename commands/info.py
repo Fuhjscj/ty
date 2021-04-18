72c3d30f6631c5bd452ff121e563544c82ca0162
Fuhjscj <81225500+Fuhjscj@users.noreply.github.com>
2021-04-18 22:04:39 +0700

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -26,23 +26,23 @@ async def info_wrapper(message: Message, **kwargs):
 
     text = f"""
     🤑☺️ DМL LP ❤️ v{__version__}
-    🔴 Ключ рукаптчи: {"✅" if db.ru_captcha_key else "❌"}
-    🟠 Удаление уведомлений: {"✅" if db.delete_all_notify else "❌"}
-    🟡 Выключение уведомлений: {"✅" if db.disable_notifications else "❌"}
-    🟢 В игноре: {len(db.ignored_members)}
-    🔵 В глобальном игноре: {len(db.ignored_global_members)}
-    🟣 В муте: {len(db.muted_members)}
-    🟤 Довов: {len(db.trusted)}
-    ⚫ Алиасов: {len(db.aliases)}
-    ⚪ Шаблонов для удаления: {len(db.regex_deleter)}
-    🔴 Выходить из бесед: {"✅" if db.auto_exit_from_chat else "❌"}
-    🟠 Удалять диалог: {"✅" if db.auto_exit_from_chat_delete_chat else "❌"}
-    🟡 Добавлять пригласившего в ЧС: {"✅" if db.auto_exit_from_chat_add_to_black_list else "❌"}
-    🟢 Повторялка: {"✅" if db.repeater_active else "❌"}
-    🔵 Триггер на повторялку: {db.repeater_word}       
-    🟣 Сервисные префиксы: {' '.join(db.service_prefixes)}
-    🟤 Свои префиксы: {' '.join(db.self_prefixes) if db.self_prefixes else ''}
-    ⚫ Префиксы дежурного: {' '.join(db.duty_prefixes) if db.duty_prefixes else ''}{update_text}
+    |🔴 Ключ рукаптчи: {"✅" if db.ru_captcha_key else "❌"}
+    |🟠 Удаление уведомлений: {"✅" if db.delete_all_notify else "❌"}
+    |🟡 Выключение уведомлений: {"✅" if db.disable_notifications else "❌"}
+    |🟢 В игноре: {len(db.ignored_members)}
+    |🔵 В глобальном игноре: {len(db.ignored_global_members)}
+    |🟣 В муте: {len(db.muted_members)}
+    |🟤 Довов: {len(db.trusted)}
+    |⚫ Алиасов: {len(db.aliases)}
+    |⚪ Шаблонов для удаления: {len(db.regex_deleter)}
+    |🔴 Выходить из бесед: {"✅" if db.auto_exit_from_chat else "❌"}
+    |🟠 Удалять диалог: {"✅" if db.auto_exit_from_chat_delete_chat else "❌"}
+    |🟡 Добавлять пригласившего в ЧС: {"✅" if db.auto_exit_from_chat_add_to_black_list else "❌"}
+    |🟢 Повторялка: {"✅" if db.repeater_active else "❌"}
+    |🔵 Триггер на повторялку: {db.repeater_word}       
+    |🟣 Сервисные префиксы: {' '.join(db.service_prefixes)}
+    |🟤 Свои префиксы: {' '.join(db.self_prefixes) if db.self_prefixes else ''}
+    |⚫ Префиксы дежурного: {' '.join(db.duty_prefixes) if db.duty_prefixes else ''}{update_text}
     """.replace('    ', '')
     await edit_message(
         message,
